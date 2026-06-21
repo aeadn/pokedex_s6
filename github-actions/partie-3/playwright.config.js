a486af1b2ab2c057b04266fb63a070f28348a16a
JoumanaL <165257125+JoumanaL@users.noreply.github.com>
2026-06-21 18:18:50 +0200

    final ?

--- github-actions/partie-3/playwright.config.js
+++ github-actions/partie-3/playwright.config.js
@@ -72,7 +72,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: "npm run dev",
+        command: "npm start",
         url: baseURL,
         reuseExistingServer: !process.env.CI,
     },
